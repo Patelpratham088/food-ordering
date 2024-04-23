15d3aaad5fd3a648693237a58215a395d034fdb2
Patelpratham088 <76466890+Patelpratham088@users.noreply.github.com>
2024-04-23 13:26:21 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,10 +15,13 @@ export default function Home() {
         />
         <div className="text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni minima odit recusandae. Illum ipsa non repudiandae? Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
+             We are a dedicated team passionate about revolutionizing the way people experience food, we believe that great meals should be accessible to everyone, no matter where they are or what their dietary preferences may be.
+
+Our mission is simple: to connect hungry customers with the best restaurants in town, all from the comfort of their own homes or offices. Whether you're craving a hearty pizza, a spicy curry, or a fresh salad, we've got you covered with a diverse selection of cuisines and flavors to suit every palate.
+
+What sets us apart is our commitment to excellence in every aspect of the ordering process. From our user-friendly interface that makes ordering a breeze, to our seamless payment and delivery options, we strive to provide an exceptional dining experience from start to finish. Plus, with our innovative features like real-time order tracking and personalized recommendations, we're always working to make your experience even better.
           </p>
-          <p>At consectetur delectus ducimus est facere iure molestias obcaecati quaerat vitae voluptate? Aspernatur dolor explicabo iste minus molestiae pariatur provident quibusdam saepe?</p>
-          <p>Laborum molestias neque nulla obcaecati odio quia quod reprehenderit sit vitae voluptates? Eos, tenetur.</p>
+          
         </div>
       </section>
       <section className="text-center my-8" id="contact">
